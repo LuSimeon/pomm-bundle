051e0ddf0db0482554206916423386033b2f49e9
Sanpi <sanpi@homecomputing.fr>
2015-11-25 14:48:01 +0100

    Test property info extrator

--- tests/src/AppBundle/Controller/IndexController.php
+++ tests/src/AppBundle/Controller/IndexController.php
@@ -2,19 +2,26 @@
 
 namespace AppBundle\Controller;
 
+use \AppBundle\Model\Config;
 use \PommProject\Foundation\Session\Session;
 use \Symfony\Component\HttpFoundation\Response;
 use \Symfony\Component\Templating\EngineInterface;
+use \Symfony\Component\PropertyInfo\PropertyInfoExtractor;
 
 class IndexController
 {
     private $pomm;
     private $templating;
+    private $property;
 
-    public function __construct(EngineInterface $templating, Session $pomm)
-    {
+    public function __construct(
+        EngineInterface $templating,
+        Session $pomm,
+        PropertyInfoExtractor $property
+    ) {
         $this->pomm = $pomm;
         $this->templating = $templating;
+        $this->property = $property;
     }
 
     public function indexAction()
@@ -29,7 +36,7 @@ class IndexController
         );
     }
 
-    public function getAction(\AppBundle\Model\Config $config)
+    public function getAction(Config $config)
     {
         return new Response(
             $this->templating->render(
@@ -43,5 +50,17 @@ class IndexController
     {
         $this->pomm->getQueryManager()
             ->query('select 1 from');
+    }
+
+    public function propertyAction()
+    {
+        $info = $this->property->getTypes(Config::class, 'name');
+
+        return new Response(
+            $this->templating->render(
+                'AppBundle:Front:property.html.twig',
+                compact('info')
+            )
+        );
     }
 }
