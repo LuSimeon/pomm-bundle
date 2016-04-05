372577d0038075addfb11e3cddd2d955032c1a3f
Sanpi <sanpi@homecomputing.fr>
2016-04-05 13:18:37 +0200

    Remove app/autoload.php

--- tests/web/app.php
+++ tests/web/app.php
@@ -3,7 +3,7 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-require_once __DIR__.'/../app/autoloader.php';
+require_once __DIR__.'/../vendor/autoloader.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
